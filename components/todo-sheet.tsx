9f2b30c9ad40024c292d61f7f994a12e27d73554
nischal1101 <kaflenk123@gmail.com>
2024-11-13 12:27:34 +0545

    add todo sheet

---
+++ components/todo-sheet.tsx
@@ -0,0 +1,28 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import React from "react";
+import CreateTodoForm from "./addTodoForm";
+import { useNewTodo } from "@/store/todoStore";
+
+const TodoSheet = () => {
+  const { isOpen, onClose } = useNewTodo();
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="min-w-[28rem] space-y-4">
+        <SheetHeader>
+          <SheetTitle>Create Todo</SheetTitle>
+          <SheetDescription>Add a new Todo</SheetDescription>
+        </SheetHeader>
+        <CreateTodoForm />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default TodoSheet;
