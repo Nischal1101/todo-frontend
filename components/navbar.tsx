e8af8c435c08232b3405268fe7a221e3f6d9c2fb
nischal1101 <kaflenk123@gmail.com>
2024-11-13 12:26:44 +0545

    complete navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,27 @@
+import MaxWidthWrapper from "@/components/maxWidthWrapper";
+import { Bell, CheckCircle, User } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <div>
-     navbar 
+    <div className="sticky inset-x-0 top-0 z-50 bg-white shadow-sm">
+      <header className=" bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex gap-4 items-center justify-center">
+                <CheckCircle color="blue" size={"28"} />
+                <h1 className="text-2xl font-bold ">Todo.</h1>
+              </div>
+              <div className="  items-center gap-8 flex ">
+                <Bell />
+                <User />
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
